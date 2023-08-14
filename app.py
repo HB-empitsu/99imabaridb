7e1b9b6a8d95bcf37c367d79a14b3cd5ece30302
Beppu Hisafumi <hisafumi.beppu@gmail.com>
2023-08-15 00:35:52 +0900

    外部アクセス可

--- app.py
+++ app.py
@@ -142,4 +142,7 @@ if __name__ == "__main__":
     scheduler.start()
 
     # FlaskのWEBアプリケーションを起動
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    # 外部アクセス可
+    app.run(debug=True, host="0.0.0.0", port=5050)
